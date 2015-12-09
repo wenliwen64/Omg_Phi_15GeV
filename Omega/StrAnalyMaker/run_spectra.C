792277d0aea10c09da3aea56af0fe7bb29dac251
Liwen Wen <wenliwen64@gmail.com>
2015-12-09 15:30:40 -0800

    update on Omega/run_spectra.C

--- Omega/StrAnalyMaker/run_spectra.C
+++ Omega/StrAnalyMaker/run_spectra.C
@@ -9,7 +9,8 @@ void run_spectra(){
     //std::string datfile = "./0826_omg_15GeV.local_analysis.root";
     std::string datfile = "./0901_omg_15GeV.local_analysis.root";
     //std::string rotfile = "./0826_omgrot_15GeV.local_analysis.root";
-    std::string rotfile = "./0901_omgrot_15GeV.local_analysis.root";
+    //std::string rotfile = "./0901_omgrot_15GeV.local_analysis.root";
+    std::string rotfile = "./1130_omgrot1_15GeV.mid_analysis.root";
     std::string fpefffile = "./mcomg_fp.manyeff.histo.root";
     std::string expefffile = "./mcomg_exp.manyeff.histo.root";
     aMaker->Init(overviewfile, datfile, rotfile, fpefffile, expefffile);// feng's upstream files
